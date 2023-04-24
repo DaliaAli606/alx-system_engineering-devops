2610b36ec8298cb26517acdc155f92443beef261
DaliaAli606 <daliahamam404@gmail.com>
2023-04-24 22:23:31 +0000

    My first commit

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-""" using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
-import sys
 
 if __name__ == "__main__":
-    text = "https://jsonplaceholder.typicode.com/users/1"
-    user_req = requests.get(text)
-    to_do = requests.get(text + "/todos")
-    list_to_do = to_do.json()
-    user = user_req.json()
-    list_done = []
-    for element in list_to_do:
-            tuple_final = (element.get('userId'), user.get('username'),
-                           element.get('completed'), element.get('title'))
-            list_done.append(tuple_final)
-    dict_final = {}
-    cont = 1
-    while (cont <= 10):
-        list_dict = []
-        for element in list_done:
-            if element[0] == cont:
-                dict_list = {}
-                dict_list["task"] = element[3]
-                dict_list["completed"] = element[2]
-                dict_list["username"] = element[1]
-                list_dict.append(dict_list)
-        dict_final[str(cont)] = list_dict
-        cont += 1
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
-    with open('todo_all_employees.json', 'w') as f:
-        json.dump(dict_final, f)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
