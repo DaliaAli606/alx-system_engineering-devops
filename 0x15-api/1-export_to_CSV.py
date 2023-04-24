67bcc820a3ab97784c90cf8f1ddc1aa65bb978a1
DaliaAli606 <daliahamam404@gmail.com>
2023-04-24 22:17:38 +0000

    My first commit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
-import json
+""" using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+
+import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    text = "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
+    user_req = requests.get(text)
+    to_do = requests.get(text + "/todos")
+    list_to_do = to_do.json()
+    user = user_req.json()
+    list_done = []
+    for element in list_to_do:
+        if element.get('userId') == int(sys.argv[1]):
+            tuple_final = (sys.argv[1], user.get('username'),
+                           element.get('completed'), element.get('title'))
+            list_done.append(tuple_final)
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+    with open(sys.argv[1]+'.csv', 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for el in list_done:
+            writer.writerow((el[0], el[1], el[2], el[3]))
